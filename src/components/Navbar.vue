d47e7f713031f015c9cec90fe1e27726ca5d858c
Sumer Patel <sumerdpatel@gmail.com>
2020-08-21 13:22:07 +0530

    navbar & sound

---
+++ src/components/Navbar.vue
@@ -0,0 +1,60 @@
+<template>
+  <nav>
+    <v-app-bar app class="blue-grey lighten-1">
+      <v-app-bar-nav-icon v-on:click="clickDrawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>
+        <span class="font-weight-black">SOUNDBOARD</span><span class="font-weight-light">APP</span>
+      </v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer app class="blue-grey lighten-1" v-model="isDrawerOpen">
+      <v-list nav>
+
+        <v-list-item v-on:click="navigate('/')">
+          <v-list-item-icon>
+            <v-icon>home</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Home</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item v-on:click="navigate('about')">
+          <v-list-item-icon>
+            <v-icon>info</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>About Us</v-list-item-title>
+        </v-list-item>
+
+        <v-list-item v-on:click="navigate('support')">
+          <v-list-item-icon>
+            <v-icon>live_help</v-icon>
+          </v-list-item-icon>
+          <v-list-item-title>Support</v-list-item-title>
+        </v-list-item>
+
+      </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+  data() {
+    return {
+      isDrawerOpen: false
+    }
+  },
+  methods: {
+    clickDrawer() {
+      this.isDrawerOpen = true;
+    },
+    navigate(navi) {
+      this.$router.push(navi);
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Sound.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-container fluid grid-list-lg>
+    <v-layout wrap>
+      <v-flex xs4 v-for="(sound, index) in sounds" :key="index">
+        <v-card v-on:click="play(sound.file)">
+          <v-card-text class="d-flex flex-column justify-center align-center">
+            <v-icon color="blue-grey darken-2" x-large>play_arrow</v-icon>
+            <p>{{ sound.title }}</p>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'Sound',
+  data() {
+    return {
+      sounds: [
+        {
+          title: 'Ambulance',
+          file: 'ambulance.mp3'
+        },
+        {
+          title: 'Bell',
+          file: 'bell.mp3'
+        },
+        {
+          title: 'Bullet',
+          file: 'bullet.mp3'
+        },
+        {
+          title: 'Cow',
+          file: 'cow.mp3'
+        },
+        {
+          title: 'Explosion',
+          file: 'explosion.mp3'
+        },
+        {
+          title: 'Fizzle',
+          file: 'fizzle.mp3'
+        },
+        {
+          title: 'Movie',
+          file: 'movie.mp3'
+        },
+        {
+          title: 'Punch',
+          file: 'punch.mp3'
+        },   
+        {
+          title: 'telephone',
+          file: 'telephone.mp3'
+        },
+      ]
+    }
+  },
+  methods: {
+    play(name) {
+      const audio = new Audio(require('@/assets/sounds/' + name));
+      audio.play();
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
