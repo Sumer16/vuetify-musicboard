057822c8aa7a8f8e025f6a5b04f132ffd79d5c71
Sumer Patel <sumerdpatel@gmail.com>
2020-08-21 13:46:23 +0530

    added content to views

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 class="text-center pa-5">About Us</h1>
+    <div class="pa-8 text-justify mb-10">
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</p>
+    </div>
+    <div class="text-center pa-4">
+      <p>All Rights Reserved.</p>
+    </div>
   </div>
 </template>
--- src/views/Support.vue
+++ src/views/Support.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="support">
-    <h1>This is the support page</h1>
+    <h1 class="text-center pa-5">Support</h1>
+    <div class="pa-8 text-justify mb-16">
+      <p class="font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Harum maiores modi quidem veniam, expedita quis laboriosam, ullam facere adipisci, iusto, voluptate sapiente corrupti asperiores rem nemo numquam fuga ab at.</p>
+    </div>
+    <div class="text-center pa-4">
+      <p>All Rights Reserved.</p>
+    </div>
   </div>
 </template>
 
